d7956e69912c88ed3eae6f20f18a5c6fabd0ac61
bvdaniel <beltranvarasdaniel@gmail.com>
2022-09-20 17:27:47 -0400

    update linked_list commit

--- arrays.py
+++ arrays.py
@@ -1,2 +1,19 @@
 new_list = [1,2,3]
 result = new_list[0]
+
+if 1 in new_list: print(True)
+"""
+Linear Seach
+"""
+for n in new_list:
+    if n == 1:
+        print(True)
+        break
+
+# Adding to a list
+
+new_list.append()
+
+# Extending a list
+
+new_list.extend([1,'d',4])
--- binary_search.py
+++ binary_search.py
@@ -7,6 +7,7 @@ def binary_search(list, target):
     
     while first <= last:
         midpoint = (first +last)//2
+         
         if list[midpoint] == target:
             return midpoint
         elif list[midpoint] < target:
